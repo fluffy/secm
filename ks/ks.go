24048c0280a089194c21662a3ffec848039fcc9e
Cullen Jennings <fluffy@iii.ca>
2015-05-23 08:22:25 -0600

    fetch key from DB

--- ks/ks.go
+++ ks/ks.go
@@ -4,20 +4,21 @@ import (
         "database/sql"
 	_ "github.com/lib/pq"
         "log"
+	"fmt"
 	"os"
 )
 
 var db *sql.DB
 
-func setupDatabase( pgPassword string ) {
+func setupDatabase( pgPassword string ) { // todo pass in hostname, port, username
         var err error
 
         // set up DB
-        db, err := sql.Open("postgres", "password='"+pgPassword+"' user=postgres dbname=postgres host=162.209.75.246 port=5432 sslmode=disable")
+        db, err = sql.Open("postgres", "password='"+pgPassword+"' user=postgres dbname=postgres host=162.209.75.246 port=5432 sslmode=disable")
 	if err != nil {
 		log.Fatal(err)
 	}
-
+	
 	sqlSetup := `
         CREATE TABLE IF NOT EXISTS keys ( kID BIGINT NOT NULL, kVal bytea NOT NULL ,  oID BIGINT NOT NULL, PRIMARY KEY( kID ) );
         CREATE TABLE IF NOT EXISTS keyUsers ( kID BIGINT NOT NULL, uID BIGINT NOT NULL , PRIMARY KEY( kID,uID ) );
@@ -29,12 +30,43 @@ func setupDatabase( pgPassword string ) {
                 log.Printf("%q\n", err)
         }
 
+	err = db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+	
 	log.Printf("Setup Database\n")
 }
 
+
+func getKey( keyID, userID string ) {
+	stmt, err := db.Prepare(
+		"SELECT keys.kVal  FROM keyUsers JOIN keys ON  keys.kID = keyUsers.kID WHERE keyUsers.uID = $2 AND keyUsers.kID = $1")
+	if err != nil {
+		log.Println("sql fatal error in getKey prep")
+		log.Fatal(err)
+	}
+	var keyVal string
+	err = stmt.QueryRow(keyID,userID).Scan( &keyVal ) 
+	if err != nil {
+		log.Println("sql fatal error in getKey querry")
+		log.Fatal(err)
+	}
+	fmt.Println(keyVal)
+}
+
+
 func main() {
+	var err error
+	
 	var pgPassword string = os.Getenv("SECM_DB_SECRET")
 	setupDatabase(pgPassword)
         defer db.Close()
 
+	err = db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+	
+	getKey( "101", "1" )
 }
