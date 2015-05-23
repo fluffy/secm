534076b97983548f514980f5af020dd02469cb72
Cullen Jennings <fluffy@iii.ca>
2015-05-23 06:44:28 -0600

    check vars

--- ks/createMachine.sh
+++ ks/createMachine.sh
@@ -5,4 +5,19 @@ if [ -z "$SECM_DB_SECRET" ]; then
     exit;
 fi
 
+if [ -z "$OS_USERNAME" ]; then
+    echo need to set OS_USERNAME with rackspace username;
+    exit;
+fi
+
+if [ -z "$OS_API_KEY" ]; then
+    echo need to set OS_API_KEY  with rackspace API_KEY;
+    exit;
+fi
+
+if [ -z "$OS_REGION_NAME" ]; then
+    echo need to set OS_REGION_NAME with rackspace REGION_NAME;
+    exit;
+fi
+
 
