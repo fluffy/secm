2393a98e80e6c4bfeb0cb7c85fdea0b152ad09eb
Cullen Jennings <fluffy@iii.ca>
2015-05-25 16:59:46 -0600

    add template version of apache config

--- createMachine.sh
+++ createMachine.sh
@@ -41,14 +41,21 @@ docker-machine ssh $MAC_NAME "docker build -t fluffy/ws:v1 /root/secm/ws"
 docker `docker-machine config $MAC_NAME` run -p 5432:5432 --name my-postgres -e POSTGRES_PASSWORD=$SECM_DB_SECRET -d postgres
 # TODO - remove public port
 docker `docker-machine config $MAC_NAME` run -p 8080:8080  --name="my-ks" --link my-postgres:db -d fluffy/ks:v1
-docker `docker-machine config $MAC_NAME` run -p 80:80 -p 443:443 --name="my-ws" --link my-ks:ks -d fluffy/ws:v1
+# TODO remove the ssh port 22
+docker `docker-machine config $MAC_NAME` run -p 80:80 -p 443:443 -p 8022:22  -v /root/data:/data --name="my-ws" --link my-ks:ks -d fluffy/ws:v1
+
+docker-machine ssh $MAC_NAME mkdir /root/data
+cat site.crt | docker-machine ssh $MAC_NAME "cat > /root/data/site.crt"
+cat site.key | docker-machine ssh $MAC_NAME "cat > /root/data/site.key"
+cat site.conf | \
+    sed -e "s/SERVER_NAME/$SECM_KS_NAME/g" | \
+    sed -e "s/OIDC_Client_ID/$OIDC_Client_ID/g" | \
+    sed -e "s/OIDC_Client_Secret/$OIDC_Client_Secret/g" | \
+    sed -e "s/OIDC_Crypto_Passphrase/$OIDC_Crypto_Passphrase/g" | \
+    docker-machine ssh $MAC_NAME "cat > /root/data/site.conf"
 
 echo
 echo Machine IP is `docker-machine ip $MAC_NAME`
 echo Remember to delete server at https://mycloud.rackspace.com
-
-
-
-
 
 
