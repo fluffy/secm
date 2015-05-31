68c2029366a3dd92b9d2f86bca0593469cc2055b
Cullen Jennings <fluffy@iii.ca>
2015-05-31 17:18:37 -0600

    cleaned up test sequence

--- client/secm.js
+++ client/secm.js
@@ -1,45 +1,118 @@
+/*jslint  browser: true, devel: true,  vars: true, todo: true, white: true, bitwise: true */
+/*global  $,WebSocket */
+/* jshint strict: true, jquery: true */
+
+var Fluffy;
+Fluffy = Fluffy || {}; // setup namespace
+
+Fluffy.SecM = (function () {
+    "use strict";
+    
+    function setup() {
+        if (!crypto.subtle) {
+            alert("No WebCrypto subtle support");
+            return;
+        }
+    }
+
+    function genKey( f ) {
+        crypto.subtle.generateKey(
+            { name : "AES-GCM", length:128 },
+            true,
+            ["encrypt","decrypt"]
+        ).then(
+            function(key) {
+                var myKey = key;
+                console.log( "gen key: " + key );
+                
+                crypto.subtle.exportKey(
+                    "jwk",
+                    myKey
+                ).then(function(ekey) {
+                    var sKey = JSON.stringify(ekey)
+                    console.log( "Exported Key: " + sKey );
+                    f( sKey );
+                }).catch( function(err) {
+                    console.log( "problem exporting key: " + err );
+                });
+                
+            }).catch( function(err) {
+                console.log( "problem generating key: " + err );
+            });
+    }
+        
+    var publicExport = {
+        setup: setup,
+        genKey: genKey
+    };
+    
+    return publicExport;
+}());
 
 $(document).ready(function(){
-    $("#createID").click(function(){
-        $.post(   $("#ksUrlID").val() + "v1/key",
+    "use strict";
+    Fluffy.SecM.setup();
+
+    $("#genBut").click(function(){
+        Fluffy.SecM.genKey( function(key) { $("#uKeyIn").val( key ) } );
+        Fluffy.SecM.genKey( function(key) { $("#cKeyIn").val( key ) } );
+    });
+
+    $("#storeKeyBut").click(function(){
+        $.post(   $("#ksUrl").val() + "v1/key",
         {
-            keyVal: $("#inValID").val(),
-            iKeyVal: $("#inVal2ID").val()
+            keyVal: $("#uKeyIn").val(),
+            iKeyVal: $("#cKeyIn").val()
         },
         function(data,status){
            var d = jQuery.parseJSON( data );
            $("#keyID").val( d.keyID );
         });
-  });
-  $("#getKeyID").click(function(){
-        $.get( $("#ksUrlID").val() + "v1/key/" + $("#keyID").val() ,
-        function(data,status){
-           $("#outKeyValID").text( data );
-        });
     });
-  $("#getIKeyID").click(function(){
-        $.get( $("#ksUrlID").val() + "v1/iKey/" + $("#keyID").val() ,
-        function(data,status){
-           $("#outIKeyValID").text( data );
-        });
-  });
-
-  $("#postMsgID").click(function(){
-        $.ajax({ type: "POST",
-                 url : $("#msUrlID").val() + "v1/ch/" + $("#keyID").val(),
-                 data: $("#msgInVal").val(),
+    
+    $("#fetchUKeyBut").click(function(){
+        $.get( $("#ksUrl").val() + "v1/key/" + $("#keyID").val() ,
+               function(data,status){
+                   $("#uKeyOut").val( data );
+               });
+    });
+    
+    $("#fetchCKeyBut").click(function(){
+        $.get( $("#ksUrl").val() + "v1/iKey/" + $("#keyID").val() ,
+               function(data,status){
+                   $("#cKeyOut").val( data );
+               });
+    });
+    
+    $("#encMsgBut").click(function(){
+        $("#msgEnc").val(  "enc-" + $("#msgIn").val() )
+    });
+    
+    $("#signMsgBut").click(function(){
+         $("#msgSign").val(  "sign-" + $("#msgEnc").val() )
+    });
+    
+    $("#postMsgBut").click(function(){
+          $.ajax({ type: "POST",
+                 url : $("#msUrl").val() + "v1/ch/" + $("#keyID").val(),
+                 data: $("#msgSign").val(),
                  success: function(data,status){
                      var d = jQuery.parseJSON( data );
-                     $("#seqNumID").val( d.seqNum );
+                     $("#seqNum").val( d.seqNum );
                  },
                  contentType: false
         });
-  });
-
-  $("#getMsgID").click(function(){
-        $.get( $("#msUrlID").val() + "v1/msg/" + $("#keyID").val() + "-" + $("#seqNumID").val() ,
-        function(data,status){
-           $("#outValID").text( data );
-        });
-  });
+    });
+    
+    $("#fetchMsgBut").click(function(){
+        $.get( $("#msUrl").val() + "v1/msg/" + $("#keyID").val() + "-" + $("#seqNum").val() ,
+               function(data,status){
+                   $("#msgOut").val( data );
+               });
+    });
+    
+    $("#decryptMsgBut").click(function(){
+         $("#msgOutDecrypt").val(  "decrypt-" + $("#msgOut").val() )
+    });
+                         
 });
